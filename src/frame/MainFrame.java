5deb0697a2d3e80de798dd1e0690a89eff7a2cac
hpdh0731 <hpdh0731@hotmail.com>
2014-05-13 22:52:14 +0800

    加入Host Panel

--- src/frame/MainFrame.java
+++ src/frame/MainFrame.java
@@ -32,6 +32,7 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(TutorialPanel.getInstance(), "tutorial");
         this.add(ChoseCharacterPanel.getInstance(), "character");
         this.add(PlayingPanel.getInstance(), "playing");
+        this.add(HostPanel.getInstance(), "host");
     }
     
     public static MainFrame getInstance()
